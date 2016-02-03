b5916ad55ef2bb4aab1ab4144dc475031dcca712
YingzhengMa <yma004@drury.edu>
2016-02-03 14:32:20 -0600

    Try to fix the remote repository

--- CoreNavigator/app/src/main/java/corenavigator/edu/drury/mcs/corenavigator/Course.java
+++ CoreNavigator/app/src/main/java/corenavigator/edu/drury/mcs/corenavigator/Course.java
@@ -7,69 +7,69 @@ import com.google.android.gms.maps.model.LatLng;
  *  building, day, and beginTime. Later iterations may expand upon this.
  */
 public class Course {
-    //basic fields
-    private String name;
-    private LatLng building;
-    private String days;
-    private String beginTime;
+	//basic fields
+	private String name;
+	private LatLng building;
+	private String days;
+	private String beginTime;
 
-    // days of the week as constants.
-    protected static final String MONDAY = "Mon";
-    protected static final String TUESDAY = "Tues";
-    protected static final String WEDNESDAY = "Wed";
-    protected static final String THURSDAY = "Thurs";
-    protected static final String FRIDAY = "Fri";
-    protected static final String SATURDAY = "Sat";
-    protected static final String SUNDAY = "Sun";
+	// days of the week as constants.
+	protected static final String MONDAY = "Mon";
+	protected static final String TUESDAY = "Tues";
+	protected static final String WEDNESDAY = "Wed";
+	protected static final String THURSDAY = "Thurs";
+	protected static final String FRIDAY = "Fri";
+	protected static final String SATURDAY = "Sat";
+	protected static final String SUNDAY = "Sun";
 
-    // Drury buildings as coordinate constants.
-    protected static final LatLng PEARSON = new LatLng(37.219146, -93.286636);
-    protected static final LatLng TSC = new LatLng(37.215706, -93.286456);
-    protected static final LatLng BURNHAM = new LatLng(37.218480, -93.286673);
-    protected static final LatLng HAMMONS = new LatLng(37.215567, -93.285314);
+	// Drury buildings as coordinate constants.
+	protected static final LatLng PEARSON = new LatLng(37.219146, -93.286636);
+	protected static final LatLng TSC = new LatLng(37.215706, -93.286456);
+	protected static final LatLng BURNHAM = new LatLng(37.218480, -93.286673);
+	protected static final LatLng HAMMONS = new LatLng(37.215567, -93.285314);
 
-    /**
-     *  Constructor for the class. Takes in parameters for each field and assigns them.
-     */
-    public Course(String name, LatLng building, String days, String beginTime) {
-        this.name = name;
-        this.building = building;
-        this.days = days;
-        this.beginTime = beginTime;
-    }
+	/**
+	 *  Constructor for the class. Takes in parameters for each field and assigns them.
+	 */
+	public Course(String name, LatLng building, String days, String beginTime) {
+		this.name = name;
+		this.building = building;
+		this.days = days;
+		this.beginTime = beginTime;
+	}
 
-    //getters and setters for each field. Setters are unused in current iteration and may not be used
-    //in the future, and so are at risk of removal pending further design planning. Setters could be used if
-    //we allow the user to correct mistakes in their schedule or update it.
-    public String getName() {
-        return name;
-    }
+	//getters and setters for each field. Setters are unused in current iteration and may not be used
+	//in the future, and so are at risk of removal pending further design planning. Setters could be used if
+	//we allow the user to correct mistakes in their schedule or update it.
+	public String getName() {
+		return name;
+	}
 
-    public LatLng getBuilding() {
-        return building;
-    }
+	public LatLng getBuilding() {
+		return building;
+	}
 
-    public String getDays() {
-        return days;
-    }
+	public String getDays() {
+		return days;
+	}
 
-    public String getBeginTime() {
-        return beginTime;
-    }
+	public String getBeginTime() {
+		return beginTime;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setBuilding(LatLng building) {
-        this.building = building;
-    }
+	public void setBuilding(LatLng building) {
+		this.building = building;
+	}
 
-    public void setDays() {
-        this.days = days;
-    }
+	public void setDays() {
+		this.days = days;
+	}
 
-    public void setBeginTime() {
-        this.beginTime = beginTime;
-    }
+	public void setBeginTime() {
+		this.beginTime = beginTime;
+	}
 }
--- CoreNavigator/app/src/main/java/corenavigator/edu/drury/mcs/corenavigator/Schedule.java
+++ CoreNavigator/app/src/main/java/corenavigator/edu/drury/mcs/corenavigator/Schedule.java
@@ -9,47 +9,47 @@ import java.util.List;
  * future.
  */
 public class Schedule{
-    //Name field and course list
+	//Name field and course list
 	private String name;
 	private List<Course> courses;
 
-    //constructor simply makes a Schedule with a given name and no courses. Courses must be added on at a time
-    // using the add function.
+	//constructor simply makes a Schedule with a given name and no courses. Courses must be added on at a time
+	// using the add function.
 	public Schedule(String name){
 		this.name = name;
 		courses = new ArrayList<Course>();
 	}
 
-    //getter for name
+	//getter for name
 	public String getName(){
 		return name;
 	}
 
-    //getter for course list
+	//getter for course list
 	public List<Course> getCourses(){
 		return courses;
 	}
 
-    /**
-     * This is a filtering function that only selects courses that have the given day as its day,
-     * and then returns the remaining list.
-     *
-     * @param day - The day that is to be filtered on. Use the constants built into the Course class
-     * @return - A list of courses that meet on the given day
-     */
+	/**
+	 * This is a filtering function that only selects courses that have the given day as its day,
+	 * and then returns the remaining list.
+	 *
+	 * @param day - The day that is to be filtered on. Use the constants built into the Course class
+	 * @return - A list of courses that meet on the given day
+	 */
 	public List<Course> getCourseOnDay(String day){
-			List<Course> dayCourses = new ArrayList<Course>();
-			for(int i=0; i<courses.size(); i++){
-				Course c = courses.get(i);
-				if((c.getDays()).contains(day)) dayCourses.add(c);
-			}
-			return dayCourses;
+		List<Course> dayCourses = new ArrayList<Course>();
+		for(int i=0; i<courses.size(); i++){
+			Course c = courses.get(i);
+			if((c.getDays()).contains(day)) dayCourses.add(c);
+		}
+		return dayCourses;
 	}
 
-    /**
-     * Only means by which a course can be added to the schedule.
-     * @param c - Course to be added
-     */
+	/**
+	 * Only means by which a course can be added to the schedule.
+	 * @param c - Course to be added
+	 */
 	public void addCourse(Course c){
 		courses.add(c);
 	}
